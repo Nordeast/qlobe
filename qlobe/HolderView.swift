e273b0279eaf847440490fc990146482d5b84f4c
Ted <m4a11205@gmail.com>
2015-11-20 17:44:22 -0600

    Add example loading animation

---
+++ qlobe/HolderView.swift
@@ -0,0 +1,121 @@
+//
+//  HolderView.swift
+//  SBLoader
+//
+//  Created by Satraj Bambra on 2015-03-17.
+//  Copyright (c) 2015 Satraj Bambra. All rights reserved.
+//
+
+import UIKit
+
+protocol HolderViewDelegate:class {
+    func animateLabel()
+}
+
+class HolderView: UIView {
+    
+    let ovalLayer = OvalLayer()
+    let triangleLayer = TriangleLayer()
+    let redRectangleLayer = RectangleLayer()
+    let blueRectangleLayer = RectangleLayer()
+    let arcLayer = ArcLayer()
+    
+    var parentFrame :CGRect = CGRectZero
+    weak var delegate:HolderViewDelegate?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = Colors.clear
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    func addOval() {
+        layer.addSublayer(ovalLayer)
+        ovalLayer.expand()
+        NSTimer.scheduledTimerWithTimeInterval(0.3, target: self, selector: "wobbleOval",
+            userInfo: nil, repeats: false)
+    }
+    
+    func wobbleOval() {
+        ovalLayer.wobble()
+        // 1
+        layer.addSublayer(triangleLayer) // Add this line
+        ovalLayer.wobble()
+        
+        // 2
+        // Add the code below
+        NSTimer.scheduledTimerWithTimeInterval(0.9, target: self,
+            selector: "drawAnimatedTriangle", userInfo: nil,
+            repeats: false)
+    }
+    
+    func drawAnimatedTriangle() {
+        triangleLayer.animate()
+        NSTimer.scheduledTimerWithTimeInterval(0.9, target: self, selector: "spinAndTransform",
+            userInfo: nil, repeats: false)
+    }
+    
+    func spinAndTransform() {
+        // 1
+        layer.anchorPoint = CGPointMake(0.5, 0.6)
+        
+        // 2
+        let rotationAnimation: CABasicAnimation = CABasicAnimation(keyPath: "transform.rotation.z")
+        rotationAnimation.toValue = CGFloat(M_PI * 2.0)
+        rotationAnimation.duration = 0.45
+        rotationAnimation.removedOnCompletion = true
+        layer.addAnimation(rotationAnimation, forKey: nil)
+        
+        // 3
+        ovalLayer.contract()
+        
+        NSTimer.scheduledTimerWithTimeInterval(0.45, target: self,
+            selector: "drawRedAnimatedRectangle",
+            userInfo: nil, repeats: false)
+        NSTimer.scheduledTimerWithTimeInterval(0.65, target: self,
+            selector: "drawBlueAnimatedRectangle",
+            userInfo: nil, repeats: false)
+    }
+    
+    func drawRedAnimatedRectangle() {
+        layer.addSublayer(redRectangleLayer)
+        redRectangleLayer.animateStrokeWithColor(Colors.red)
+    }
+    
+    func drawBlueAnimatedRectangle() {
+        layer.addSublayer(blueRectangleLayer)
+        blueRectangleLayer.animateStrokeWithColor(Colors.blue)
+        NSTimer.scheduledTimerWithTimeInterval(0.40, target: self, selector: "drawArc",
+            userInfo: nil, repeats: false)
+    }
+    
+    func drawArc() {
+        layer.addSublayer(arcLayer)
+        arcLayer.animate()
+        NSTimer.scheduledTimerWithTimeInterval(0.90, target: self, selector: "expandView",
+            userInfo: nil, repeats: false)
+    }
+    
+    func expandView() {
+        backgroundColor = Colors.blue
+        frame = CGRectMake(frame.origin.x - blueRectangleLayer.lineWidth,
+            frame.origin.y - blueRectangleLayer.lineWidth,
+            frame.size.width + blueRectangleLayer.lineWidth * 2,
+            frame.size.height + blueRectangleLayer.lineWidth * 2)
+        layer.sublayers = nil
+        
+        UIView.animateWithDuration(0.3, delay: 0.0, options: UIViewAnimationOptions.CurveEaseInOut, animations: {
+            self.frame = self.parentFrame
+            }, completion: { finished in
+                self.addLabel()
+        })
+    }
+    
+    func addLabel() {
+        delegate?.animateLabel()
+    }
+    
+}
--- qlobe/LoadMainViewController.swift
+++ qlobe/LoadMainViewController.swift
@@ -17,12 +17,19 @@
 import UIKit
 import Parse
 
-class loadMainViewController: UIViewController{
-
+class loadMainViewController: UIViewController, HolderViewDelegate{
+    
+    // timer to control the animation
+    var timerCount   = 1
+    var timer        = NSTimer()
+    var timerRunning = false
+    
+    // animation object
+    var holderView = HolderView(frame: CGRectZero)
     
     //create new pfQuery - This is the bridge between our app and Parse: "trivia" is our class name on Parse
     let queryTrivia: PFQuery = PFQuery(className:"trivia")
-    var didLoad = true
+    var didLoad = false
     
     
     
@@ -31,15 +38,18 @@ class loadMainViewController: UIViewController{
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = UIColor(netHex:0x2c3e50)
-        label1.font = UIFont(name: "Kankin", size: 80.0)
-        label1.textColor = UIColor(netHex: 0xeeeeee)
-        label1.textAlignment = NSTextAlignment.Center
+        //        view.backgroundColor = UIColor(netHex:0x2c3e50)
+        //        label1.font = UIFont(name: "Kankin", size: 80.0)
+        //        label1.textColor = UIColor(netHex: 0xeeeeee)
+        //        label1.textAlignment = NSTextAlignment.Center
+        label1.text = ""
+        
         
         //retrieve data from parse query
         retrieveTrivia()
-
-
+        
+        //Start the loading animation
+        addHolderView()
     }
     
     override func didReceiveMemoryWarning() {
@@ -55,17 +65,17 @@ class loadMainViewController: UIViewController{
             
             // Loop through the objects array
             for triviaObject in objects!{
-                if(self.label1.text == "Loading..."){
-                    self.label1.text = "Loadng"
-                }else if self.label1.text == "Loading"{
-                    self.label1.text = "Loadng."
-                }else if self.label1.text == "Loading."{
-                    self.label1.text = "Loadng.."
-                }else if self.label1.text == "Loading.."{
-                    self.label1.text = "Loadng..."
-                }else {
-                    self.label1.text = "Loadng..."
-                }
+                //                if(self.label1.text == "Loading..."){
+                //                    self.label1.text = "Loadng"
+                //                }else if self.label1.text == "Loading"{
+                //                    self.label1.text = "Loadng."
+                //                }else if self.label1.text == "Loading."{
+                //                    self.label1.text = "Loadng.."
+                //                }else if self.label1.text == "Loading.."{
+                //                    self.label1.text = "Loadng..."
+                //                }else {
+                //                    self.label1.text = "Loadng..."
+                //                }
                 // Retrieve data for each object (key, question, ans2, ans3, correctAns)
                 let triviaQuest : String? = (triviaObject as PFObject)["question"] as? String
                 let triviaAns2 : String? = (triviaObject as PFObject)["ans2"] as? String
@@ -77,7 +87,7 @@ class loadMainViewController: UIViewController{
                 if ( triviaKey != nil && triviaQuest != nil && triviaAns2 != nil &&  triviaAns3 != nil && triviaAns != nil){
                     let trivia = triviaQuestion(Key: triviaKey!, Question: triviaQuest!, Answer: triviaAns!, WrongAnswer:  triviaAns2!, WrongAnswer2:  triviaAns3!)
                     triviaQuestions.append(trivia) // append to the global array of trivia questions
-                    
+                    self.didLoad = true
                     
                 }else{
                     self.label1.text = "Network Error"
@@ -90,18 +100,87 @@ class loadMainViewController: UIViewController{
             for element in triviaQuestions{
                 print(element.Key, element.Question)
             }
-            
-                        if (self.didLoad == true) {
-                            //perform segue to View Controller : Main menu
-                            self.performSegueWithIdentifier("finnishLoad", sender: self)
-                        }
+            //            if (didLoad == true) {
+            //                //perform segue to View Controller : Main menu
+            //                self.performSegueWithIdentifier("finnishLoad", sender: self)
+            //            }
             
         }
-        
-        
     }
     
     
+    // creat animation
+    func addHolderView() {
+        let boxSize: CGFloat = 100.0
+        holderView.frame = CGRect(x: view.bounds.width / 2 - boxSize / 2,
+            y: view.bounds.height / 2 - boxSize / 2,
+            width: boxSize,
+            height: boxSize)
+        holderView.parentFrame = view.frame
+        holderView.delegate = self
+        view.addSubview(holderView)
+        holderView.addOval()
+    }
+    
+    
+    // animation content
+    func animateLabel() {
+        // 1
+        holderView.removeFromSuperview()
+        view.backgroundColor = Colors.blue
+        
+        // 2
+        let label: UILabel = UILabel(frame: view.frame)
+        label.textColor = Colors.white
+        //label.font = UIFont(name: "HelveticaNeue-Thin", size: 120.0)
+        label.font = UIFont(name: "Kankin", size: 120.0)
+        label.textAlignment = NSTextAlignment.Center
+        label.text = "qLobe"
+        label.transform = CGAffineTransformScale(label.transform, 0.25, 0.25)
+        view.addSubview(label)
+        
+        
+        // 3
+        //        UIView.animateWithDuration(0.4, delay: 0.0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.1, options: UIViewAnimationOptions.CurveEaseInOut,
+        //            animations: ({
+        //                label.transform = CGAffineTransformScale(label.transform, 4.0, 4.0)
+        //            }), completion: { finished in
+        //                self.addButton()
+        //        })
+        
+        //3
+        UIView.animateWithDuration(0.4, delay: 0.0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.1, options: UIViewAnimationOptions.CurveEaseInOut,
+            animations: ({
+                label.transform = CGAffineTransformScale(label.transform, 4.0, 4.0)
+            }), completion: nil)
+        
+        runTimer()
+    }
+    
+    // timer function
+    func Counting(){
+        print(timerCount)
+        if(timerCount > 0){
+            timerCount -= 1
+        }
+        else{
+            timerRunning = false
+            timerCount   = 1
+            if ( didLoad == true) {
+                //perform segue to View Controller : Main menu
+                timer.invalidate()
+                self.performSegueWithIdentifier("finnishLoad", sender: self)
+            }
+        }
+    }
+    
+    func runTimer(){
+        if(timerRunning == false){
+            // run the timer
+            timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("Counting"), userInfo: nil, repeats: true)
+            timerRunning = true
+        }
+    }
 }
 
 
